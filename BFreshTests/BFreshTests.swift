79de91f3a2fa80c0a6551747bf8c029db045d13e
anagha166 <anaghakamath@gmail.com>
2025-04-05 15:35:59 -0700

    Initial Commit

---
+++ BFreshTests/BFreshTests.swift
@@ -0,0 +1,17 @@
+//
+//  BFreshTests.swift
+//  BFreshTests
+//
+//  Created by Anagha Kamath on 4/5/25.
+//
+
+import Testing
+@testable import BFresh
+
+struct BFreshTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
